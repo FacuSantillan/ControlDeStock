d02ceea9f2a29209ad69025fce21c8dff4fa28d8
Facundo <facusantillan1@gmail.com>
2024-04-27 23:25:55 -0300

    /

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import { useState } from 'react';
 import { Route, Routes, useLocation} from 'react-router-dom';
 
+//Componentes
 import NavBar from './components/1-NavBar/navBar';
+import SearchBar from './components/2-SearchBar/searchBar';
+
+//Vistas
+
 
 import './App.css';
 
@@ -13,10 +18,11 @@ function App() {
     
     <div>
           {pathname != "/" && <NavBar/>}
+          {pathname === "/Vender" && <SearchBar/>}
 
     <Routes>
 
-
+      
 
     </Routes>
 
