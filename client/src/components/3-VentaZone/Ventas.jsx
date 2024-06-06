6f8269aec3cc6eba07af3c32aa7a6eff5635f00f
Facundo <facusantillan1@gmail.com>
2024-06-06 12:43:16 -0300

    dsa

--- client/src/components/3-VentaZone/Ventas.jsx
+++ client/src/components/3-VentaZone/Ventas.jsx
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
 import { FaTimes } from 'react-icons/fa';
-import productos from './productos'; // Importa el objeto de productos
+import { AiOutlineDelete } from "react-icons/ai";
+import { IoIosArrowForward } from "react-icons/io";
+
+import productos from './productos';
 import "./Ventas.css";
 
 export default function Ventas() {
-    const [cantidades, setCantidades] = useState({}); // Estado para almacenar las cantidades de cada producto
-    const [productosSeleccionados, setProductosSeleccionados] = useState(productos); // Estado para almacenar los productos seleccionados
+    const [cantidades, setCantidades] = useState({});
+    const [descuento, setDescuento] = useState(0);
+    const [productosSeleccionados, setProductosSeleccionados] = useState(productos);
 
     const handleCantidadChange = (event, productId) => {
         const nuevaCantidad = event.target.value;
         setCantidades({ ...cantidades, [productId]: nuevaCantidad });
     };
-    
 
     const handleEliminarProducto = (productId) => {
         const productosActualizados = productosSeleccionados.filter(producto => producto.id !== productId);
@@ -22,52 +25,71 @@ export default function Ventas() {
     };
 
     const handleTotalItems = () => {
-        return productosSeleccionados.length
+        return productosSeleccionados.length;
     }
 
-    let preciosTotales = 0;
-   
+    let subtotal = 0;
+
     const calcularPrecioTotal = (precio, cantidad) => {
-        let total = (parseFloat(precio.replace('$', '')) * cantidad).toFixed(2);
-        preciosTotales = preciosTotales + parseFloat(total);
-        return total
+        const total = parseFloat(precio.replace('$', '')) * cantidad;
+        subtotal += total;
+        return total.toFixed(2);
     };
 
     const handleTotalPrecio = () => {
-        let totalPrecio = 0;
-            totalPrecio += preciosTotales;
-        return totalPrecio; 
+        return subtotal.toFixed(2);
     };
 
-    
+    const handleTotalDescuento = () => {
+        const precio = handleTotalPrecio();
+        const descuentoAplicado = precio * (descuento / 100);
+        const totalConDescuento = precio - descuentoAplicado;
+        return totalConDescuento.toFixed(2);
+    };
+
     return (
         <div className='container-ventas'>
-            <div className='container-productos-list'> 
-            {productosSeleccionados.map((producto) => (
-                
-                <div className='venta-producto' key={producto.id}>
-
-                    <input type='number' value={cantidades[producto.id] || 1} onChange={(e) => handleCantidadChange(e, producto.id)} />
-                    
-                    <div className='productos-information'>
-                        <u className='producto-nombre'>{producto.nombre}</u>
-                        <u className='cantidad-stock'>{producto.stock}</u>
-                        <u className='precio-prducto'>${calcularPrecioTotal(producto.precio, cantidades[producto.id] || 1)}</u>
-                        <button className='button-delete' onClick={() => handleEliminarProducto(producto.id)}><FaTimes size={18}/></button>
+            <div className='container-productos-list'>
+                {productosSeleccionados.map((producto) => (
+                    <div className='venta-producto' key={producto.id}>
+                        <input type='number' value={cantidades[producto.id] || 1} onChange={(e) => handleCantidadChange(e, producto.id)} />
+                        <div className='productos-information'>
+                            <u className='producto-nombre'>{producto.nombre}</u>
+                            <u className='cantidad-stock'>{producto.stock}</u>
+                            <u className='precio-prducto'>${calcularPrecioTotal(producto.precio, cantidades[producto.id] || 1)}</u>
+                            <button className='button-delete' onClick={() => handleEliminarProducto(producto.id)}><FaTimes size={18} /></button>
+                        </div>
                     </div>
-
-                </div>
-            ))}
+                ))}
             </div>
 
             <div className='container-cobro'>
                 <p className='items'> Items:{handleTotalItems()}</p>
-                <p className='subtotal'>Subtotal: ${handleTotalPrecio()}</p>
-                <p className='descuento'>Descuento</p>
-                <input type='text'></input>
-                <p className='total'>Total</p>
-            </div>
 
+                <p className='subtotal'>Subtotal: </p>
+                <p className='subtotal-numero'> ${handleTotalPrecio()}</p>
+
+                <p className='descuento'>Descuento</p>
+                <input type='number' value={descuento} onChange={(e) => setDescuento(e.target.value)} />
+
+                <p className='signo'>%</p>
+                <p className='total'>Total: </p>
+                <p className='total-numero'>${handleTotalDescuento()}</p>
+
+                <div>
+
+                <button className='button-clear-carry' >
+                    <AiOutlineDelete size={30}/>
+                </button>
+
+                <button className='button-pay' >
+                       ir al pago
+                          
+                    </button>
+
+                    <IoIosArrowForward className='icon' size={23}/>
+                </div>
+            </div>
         </div>
     );
 }
